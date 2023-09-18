77f2f34346cc8cd2e26c352bc2e47b698c9e037c
Aries <114472085+Ariesybs@users.noreply.github.com>
2023-09-18 17:57:46 +0800

    Update [id].js

--- pages/api/metadata/[id].js
+++ pages/api/metadata/[id].js
@@ -5,16 +5,10 @@ export default (req, res) => {
 
   // 根据 id 查询相应的 JSON 数据，这里可以模拟一个示例数据
   const jsonData = {
-      weapon_id : id,
-      name:"weapon"+id,
-      description:"this is a useful weapon,please keep it safe",
-      image:"https://www.pencilvision.top/img/box.jpg",
-      rarity:"3",
-      type:"weapon",
-      value:"5ETH",
-      weight:"",
-      durability:"",
-      animation_url:"https://pencilvision.top/model/"+id
+      "name":"weapon"+id,
+      "description":"this is a useful weapon,please keep it safe",
+      "image":"https://www.pencilvision.top/img/box.jpg",
+      "animation_url":"https://pencilvision.top/model/"+id
   };
 
   res.status(200).json(jsonData);
