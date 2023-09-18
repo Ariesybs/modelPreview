9445425f3a33cf5f5b15da869e6af581961d72cb
Aries <1575577205@qq.com>
2023-09-18 14:59:43 +0800

    Your commit message here

---
+++ pages/api/metadata/[id].js
@@ -0,0 +1,23 @@
+// pages/api/data/[id].js
+
+export default (req, res) => {
+  const { id } = req.query; // 通过 req.query 获取 URL 中的 id 参数
+
+  // 根据 id 查询相应的 JSON 数据，这里可以模拟一个示例数据
+  const jsonData = {
+    weapon_id : id,
+    metadata: {
+      name:"weapon"+id,
+      description:"this is a useful weapon,please keep it safe",
+      image_uri:"",
+      rarity:"3",
+      type:"weapon",
+      value:"5ETH",
+      weight:"",
+      durability:"",
+      animation_uri:"https://pencilvision.top/model/"+id
+    },
+  };
+
+  res.status(200).json(jsonData);
+};
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import '../public/home.css';
+const HomePage = () => {
+  return (
+    <>
+      <Head>
+      <meta charset="UTF-8"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <title>铅笔视界</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"></link>
+    <link rel="icon" href="/img/pen_logo.png"></link>
+    
+      </Head>
+      <div class="header">
+        <h1 class="animate__animated animate__fadeInLeft" >铅笔视界</h1>
+        <p class="animate__animated animate__fadeInRight">Build Your Oasis World With VR</p>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    
+    </>
+  );
+};
+
+export default HomePage;
