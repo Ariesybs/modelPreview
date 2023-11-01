608440a6aae04e452f96bc0fb2175a0581e39e81
Aries <1575577205@qq.com>
2023-11-01 21:20:12 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
-import '../globals.css';
-
+// import "../globals.css";
+import "../public/base.css";
+import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   // 这里可以添加全局逻辑，例如数据获取、全局状态等
 
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
